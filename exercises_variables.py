c17844bdbb77992e0db8e7cf30db0cd6de919ddb
sp9end <barbara.mysliwiec.86@gmail.com>
2026-04-13 14:26:47 +0200

    Add variables exercises - strings, ints, floats, booleans

---
+++ exercises_variables.py
@@ -0,0 +1,44 @@
+# Czas na ćwiczenia! 💪
+# Stwórz nowy plik exercises_variables.py i rozwiąż trzy zadania:
+
+# Ćwiczenie 1
+# Stwórz zmienne opisujące Ciebie:
+
+# imię
+# wiek
+# wzrost
+# czy lubisz kawę (True/False)
+
+# Wyświetl każdą zmienną przez print().
+
+name = "Barbara"
+age = 39
+height = 1.70
+likes_coffee = True
+
+print(name)
+print(age)
+print(height)
+print(likes_coffee)
+
+# Ćwiczenie 2
+# Dla każdej zmiennej z ćwiczenia 1 wyświetl jej typ przez print(type(...)).
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(likes_coffee))
+
+
+# Ćwiczenie 3
+# Stwórz zmienne opisujące dowolne miasto:
+
+# nazwa miasta
+city_name = "Warszawa"
+# liczba mieszkańców
+number_of_inhabitants = 1800000
+# średnia temperatura w lipcu
+average_temp_in_july = 19.0
+# czy jest stolicą (True/False)
+is_capital = True
+# Wyświetl wszystkie zmienne.
+print(f"{city_name},{number_of_inhabitants}, {average_temp_in_july}, {is_capital}")
