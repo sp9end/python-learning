2d1c35b2524b3660f21201da135d5bbcbe387339
sp9end <barbara.mysliwiec.86@gmail.com>
2026-04-13 14:41:57 +0200

    Add operators.py

---
+++ operators.py
@@ -0,0 +1,20 @@
+a = 10
+b = 3
+
+print(a + b)   # dodawanie     → 13
+print(a - b)   # odejmowanie   → 7
+print(a * b)   # mnożenie      → 30
+print(a / b)   # dzielenie     → 3.3333...
+print(a // b)  # dzielenie całkowite → 3
+print(a % b)   # reszta z dzielenia  → 1
+print(a ** b)  # potęgowanie   → 1000
+
+a = 10
+b = 3
+
+print(a > b)   # większy niż        → True
+print(a < b)   # mniejszy niż       → False
+print(a == b)  # równy              → False
+print(a != b)  # różny              → True
+print(a >= b)  # większy lub równy  → True
+print(a <= b)  # mniejszy lub równy → False
